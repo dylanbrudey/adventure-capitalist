0821b85395ae40149dcd7b4a54b05304d851961f
Kazooru <47382098+Kazooru@users.noreply.github.com>
2021-06-10 23:12:40 +0200

    add managers

--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -7,19 +7,28 @@ const productList = [{
   initialPrice: 5,
   image: 'https://www.papillesetpupilles.fr/wp-content/uploads/2012/09/Papaye-%C2%A9-Still-AB-shutterstock.jpg',
   unlockPrice: 0,
-  unlock: true
+  unlock: true,
+  managerUnlockPrice: 1000,
+  managerUnlocked: false,
+  managerImage: 'https://www.pngrepo.com/png/23036/180/man.png'
 }, {
   name: 'Ananas',
   initialPrice: 30,
   image: 'https://resize.prod.docfr.doc-media.fr/img/var/doctissimo/storage/images/media/images/fiche-ananas-wd/7850301-1-fre-FR/fiche-ananas-wd.jpg',
   unlockPrice: 300,
-  unlock: false
+  unlock: false,
+  managerUnlockPrice: 4000,
+  managerUnlocked: false,
+  managerImage: 'https://www.pngrepo.com/png/51675/180/man.png'
 }, {
   name: 'Fraise',
   initialPrice: 80,
   image: 'https://grandest.mutualite.fr/content/uploads/sites/45/2020/05/Cover-FRAISES-730x480.jpg',
   unlockPrice: 1200,
-  unlock: false
+  unlock: false,
+  managerUnlockPrice: 8000,
+  managerUnlocked: false,
+  managerImage: 'https://www.pngrepo.com/png/91859/180/man.png'
 }];
 
 const Game = () => {
--- src/pages/game/product.js
+++ src/pages/game/product.js
@@ -10,7 +10,7 @@ const Product = ({
   productIndex
 }) => {
   const {
-    image, initialPrice, unlockPrice, unlock
+    image, initialPrice, unlockPrice, unlock, managerImage
   } = product;
   const [price, setPrice] = useState(initialPrice);
   const [productMoney, setProductMoney] = useState(0);
@@ -53,42 +53,52 @@ const Product = ({
     setCompletionPurcent(Math.round((money * 100) / nextLevel.moneyRequired));
   });
   return (
-    <Row>
-      <Col>
+    <div>
+      <Row>
+        <Col>
+          <div>
+            <button type="button" className="btn" onClick={addMoney} disabled={!unlock}>
+              <div className="img-container">
+                <img src={image} className="img-fluid rounded" alt="Responsive" />
+                <div className="text-block-price">
+                  <span className="display-4">{showPrice}</span>
+                </div>
+                <div className="text-block-level">
+                  <span className="display-4">{showCurrentLevel}</span>
+                </div>
+              </div>
+            </button>
+          </div>
+        </Col>
+        {unlock ? (
+          <UnlockedProductProperties
+            showProductMoney={showProductMoney}
+            buyNextLevel={buyNextLevel}
+            showNextLevel={showNextLevel}
+            nextLevel={nextLevel}
+            completionPurcent={completionPurcent}
+          />
+        )
+          : (
+            <LockedProductProperties
+              unlockPrice={unlockPrice}
+              product={product}
+              products={products}
+              setProducts={setProducts}
+              productIndex={productIndex}
+            />
+          )}
+      </Row>
+      <Row>
         <div>
           <button type="button" className="btn" onClick={addMoney} disabled={!unlock}>
             <div className="img-container">
-              <img src={image} className="img-fluid rounded" alt="Responsive" />
-              <div className="text-block-price">
-                <span className="display-4">{showPrice}</span>
-              </div>
-              <div className="text-block-level">
-                <span className="display-4">{showCurrentLevel}</span>
-              </div>
+              <img src={managerImage} className="img-fluid rounded" alt="Responsive" />
             </div>
           </button>
-          <button type="button" className="btn btn-dark" onClick={buyNextLevel} hidden>OHOHO</button>
         </div>
-      </Col>
-      {unlock ? (
-        <UnlockedProductProperties
-          showProductMoney={showProductMoney}
-          buyNextLevel={buyNextLevel}
-          showNextLevel={showNextLevel}
-          nextLevel={nextLevel}
-          completionPurcent={completionPurcent}
-        />
-      )
-        : (
-          <LockedProductProperties
-            unlockPrice={unlockPrice}
-            product={product}
-            products={products}
-            setProducts={setProducts}
-            productIndex={productIndex}
-          />
-        )}
-    </Row>
+      </Row>
+    </div>
   );
 };
 
@@ -111,7 +121,7 @@ const UnlockedProductProperties = ({
         <div className="progress-bar bg-danger" role="progressbar" style={{ width: showCompletionPurcent }} aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"> </div>
       </div>
       <div className="card">
-        <button type="button" className="btn btn-warning" onClick={buyNextLevel} disabled={!nextLevel.activated}>
+        <button type="button" className="btn btn-warning text-white" onClick={buyNextLevel} disabled={!nextLevel.activated}>
           <span className="display-4">{showNextLevel}</span>
         </button>
       </div>
