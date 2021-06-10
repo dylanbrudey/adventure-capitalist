9123e4bdc48266bb48225114ac90ca39a9611c77
Kazooru <47382098+Kazooru@users.noreply.github.com>
2021-06-10 22:27:45 +0200

    update product styling

--- src/pages/game/product.js
+++ src/pages/game/product.js
@@ -14,17 +14,16 @@ const Product = ({
   } = product;
   const [price, setPrice] = useState(initialPrice);
   const [productMoney, setProductMoney] = useState(0);
+  const [completionPurcent, setCompletionPurcent] = useState(0);
   const [nextLevel, setNextLevel] = useState({
     level: 2,
     moneyRequired: price * 2,
     activated: false
   });
-  let completionPurcent = 0;
 
   const addMoney = () => {
     setMoney(money + price);
     setProductMoney(productMoney + price);
-    completionPurcent = Math.round((productMoney * 100) / nextLevel.moneyRequired);
   };
 
   const buyNextLevel = () => {
@@ -36,25 +35,37 @@ const Product = ({
       activated: false
     });
     setMoney(money - nextLevel.moneyRequired);
+    setCompletionPurcent(Math.round((productMoney * 100) / nextLevel.moneyRequired));
   };
+
+  const showPrice = `${price}$`;
+  const showProductMoney = `${productMoney}$`;
+  const showNextLevel = `Buy for ${nextLevel.moneyRequired}$`;
+  const showCurrentLevel = `Level ${nextLevel.level - 1}`;
 
   useEffect(() => {
     if (money >= nextLevel.moneyRequired && !nextLevel.activated) {
       setNextLevel({ ...nextLevel, activated: true });
     }
+    if (money < nextLevel.moneyRequired && nextLevel.activated) {
+      setNextLevel({ ...nextLevel, activated: false });
+    }
+    setCompletionPurcent(Math.round((money * 100) / nextLevel.moneyRequired));
   });
-  const showPrice = `${price}$`;
-  const showProductMoney = `${productMoney}$`;
-  const showNextLevel = `Level ${nextLevel.level} : ${nextLevel.moneyRequired}$`;
   return (
     <Row>
-      <Col xs={4}>
+      <Col>
         <div>
           <button type="button" className="btn" onClick={addMoney} disabled={!unlock}>
-            <div className="">
-              <img src={image} className="img-fluid" alt="Responsive" />
+            <div className="img-container">
+              <img src={image} className="img-fluid rounded" alt="Responsive" />
+              <div className="text-block-price">
+                <span className="display-4">{showPrice}</span>
+              </div>
+              <div className="text-block-level">
+                <span className="display-4">{showCurrentLevel}</span>
+              </div>
             </div>
-            <span className="display-4">{showPrice}</span>
           </button>
           <button type="button" className="btn btn-dark" onClick={buyNextLevel} hidden>OHOHO</button>
         </div>
@@ -81,6 +92,33 @@ const Product = ({
   );
 };
 
+const UnlockedProductProperties = ({
+  showProductMoney,
+  buyNextLevel,
+  showNextLevel,
+  nextLevel,
+  completionPurcent
+}) => {
+  const showCompletionPurcent = `${completionPurcent}%`;
+  return (
+    <Col className="ml-5 pl-5 text-center" xs={6}>
+      <div className="card mb-2 mt-4">
+        <div className="card-body">
+          <h5 className="card-title display-3">{showProductMoney}</h5>
+        </div>
+      </div>
+      <div className="progress mb-2">
+        <div className="progress-bar bg-danger" role="progressbar" style={{ width: showCompletionPurcent }} aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"> </div>
+      </div>
+      <div className="card">
+        <button type="button" className="btn btn-warning" onClick={buyNextLevel} disabled={!nextLevel.activated}>
+          <span className="display-4">{showNextLevel}</span>
+        </button>
+      </div>
+    </Col>
+  );
+};
+
 const LockedProductProperties = ({
   unlockPrice, products, setProducts, product, productIndex
 }) => {
@@ -97,7 +135,7 @@ const LockedProductProperties = ({
   });
   return (
     <Col className="ml-5 pl-5 text-center" xs={6}>
-      <button type="button" className="card mb-2 unlock-button" onClick={() => { setClick(!isUnlocked); }}>
+      <button type="button" className="card mb-2 mt-4 unlock-button" onClick={() => { setClick(!isUnlocked); }}>
         <div className="card-body">
           <h5 className="card-title display-3">{showUnlockPrice}</h5>
         </div>
@@ -105,33 +143,4 @@ const LockedProductProperties = ({
     </Col>
   );
 };
-
-const UnlockedProductProperties = ({
-  showProductMoney,
-  buyNextLevel,
-  showNextLevel,
-  nextLevel,
-  completionPurcent
-}) => {
-  const showCompletionPurcent = `${completionPurcent}%`;
-  console.log(`COMPLETION : ${completionPurcent}`);
-  return (
-    <Col className="ml-5 pl-5 text-center" xs={6}>
-      <div className="card mb-2">
-        <div className="card-body">
-          <h5 className="card-title display-3">{showProductMoney}</h5>
-        </div>
-      </div>
-      <div className="progress mb-2">
-        <div className="progress-bar bg-danger" role="progressbar" style={{ width: { showCompletionPurcent } }} aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"> </div>
-      </div>
-      <div className="card">
-        <button type="button" className="btn btn-warning" onClick={buyNextLevel} disabled={!nextLevel.activated}>
-          {showNextLevel}
-        </button>
-      </div>
-    </Col>
-  );
-};
-
 export default Product;
