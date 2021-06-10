57eb799ef2f146c77d585f910fe57edfbaaf7074
Kazooru <47382098+Kazooru@users.noreply.github.com>
2021-06-10 14:58:55 +0200

    update product

--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -9,14 +9,19 @@ const Product = ({
   initialPrice
 }) => {
   const [price, setPrice] = useState(initialPrice);
+  const [productMoney, setProductMoney] = useState(0);
   const [nextLevel, setNextLevel] = useState({
     level: 2,
     moneyRequired: price * 2,
-    activated: false 
+    activated: false
   });
   const addMoney = () => {
-    const { level, moneyRequired } = nextLevel;
     setMoney(money + price);
+    setProductMoney(productMoney + price);
+  };
+
+  const buyNextLevel = () => {
+    const { level, moneyRequired } = nextLevel;
     setPrice(price * 2);
     setNextLevel({
       level: level + 1,
@@ -26,19 +31,29 @@ const Product = ({
   };
   return (
     <Row>
-      <Col>
+      <Col xs={4}>
         <div>
-          <button type="button">
+          <button type="button" onClick={addMoney}>
             <img src={image} className="img-fluid" alt="Responsive" />
           </button>
         </div>
       </Col>
       <Col>
-        <div>
+        <div className="card">
+          <img className="card-img-top" src="..." alt="Card cap" />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+          </div>
         </div>
-        <div>
+        <div className="progress">
+          <div className="progress-bar bg-danger" role="progressbar" style={{ width: '100%' }} aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">Hola</div>
         </div>
-        <div>
+        <div className="card">
+          <div className="card-body">
+            <button type="button" className="btn btn-warning" onClick={buyNextLevel}>
+              HERE
+            </button>
+          </div>
         </div>
       </Col>
     </Row>
@@ -66,9 +81,9 @@ const Game = () => {
         </div>
         <button type="button" onClick={gogogo}>Hohoho</button>
         <div className="row border rounded">
-          {/* <Product money={money} setMoney={setMoney} initialPrice={5} image="https://www.papillesetpupilles.fr/wp-content/uploads/2012/09/Papaye-%C2%A9-Still-AB-shutterstock.jpg" />
+          <Product money={money} setMoney={setMoney} initialPrice={5} image="https://www.papillesetpupilles.fr/wp-content/uploads/2012/09/Papaye-%C2%A9-Still-AB-shutterstock.jpg" />
           <Product image="https://resize.prod.docfr.doc-media.fr/img/var/doctissimo/storage/images/media/images/fiche-ananas-wd/7850301-1-fre-FR/fiche-ananas-wd.jpg" />
-          <Product image="https://grandest.mutualite.fr/content/uploads/sites/45/2020/05/Cover-FRAISES-730x480.jpg" /> */}
+          <Product image="https://grandest.mutualite.fr/content/uploads/sites/45/2020/05/Cover-FRAISES-730x480.jpg" />
         </div>
       </div>
     </Page>
